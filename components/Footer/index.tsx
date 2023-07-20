89b24bbc7792da678a91129260b601ac51d79569
tetsu1615 <85781778+tetsu1615@users.noreply.github.com>
2023-07-20 16:42:08 +0900

    feat: DELETE-ALL

--- components/Footer/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './index.module.css';
-
-export default function Footer() {
-  return (
-    <footer className={styles.footer}>
-      <p className={styles.cr}>© SIMPLE. All Rights Reserved 2023</p>
-    </footer>
-  );
-}
